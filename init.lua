dba681431e82e019737926adfeb3921a17da8a6e
nop <noplab90@gmail.com>
2023-11-28 16:37:35 +0900

    change packer to lazy

--- init.lua
+++ init.lua
@@ -1,67 +1,93 @@
 -- TODO
--- LSPSaga の導入 Packerまではできている
 
+-- vim.cmd [[
+-- let g:python_host_prog='/Users/nop/.pyenv/versions/2.7.18/bin/python'
+-- let g:python3_host_prog='/Users/nop/.pyenv/shims/python3'
+-- ]]
+
+-- WSL Settings
 vim.cmd [[
-let g:python_host_prog='/Users/nop/.pyenv/versions/2.7.18/bin/python'
-let g:python3_host_prog='/Users/nop/.pyenv/shims/python3'
+let s:clip = '/mnt/c/Windows/System32/clip.exe'
+if executable(s:clip)
+    augroup WSLYank
+        autocmd!
+        autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
+    augroup END
+endif
 ]]
 
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	})
+end
 
-require("packer").startup(function(use)
-	-- plugin manager
-	use "wbthomason/packer.nvim"
 
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
 	-- colorscheme
-	use "sainnhe/sonokai"
+	'sainnhe/sonokai',
 
-	-- treesitter
-	use "nvim-treesitter/nvim-treesitter"
+	-- treesitter シンタックスハイライト
+	'nvim-treesitter/nvim-treesitter',
 
 	-- lsp config
-	use "neovim/nvim-lspconfig"
+	'neovim/nvim-lspconfig',
 	-- lsp manager
-	use "williamboman/mason.nvim"
-	use "williamboman/mason-lspconfig.nvim"
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
 
 	-- complete
-	use "hrsh7th/nvim-cmp"
-	use "hrsh7th/cmp-nvim-lsp"
-	use "hrsh7th/vim-vsnip"
-	use "hrsh7th/cmp-path"
-	use "hrsh7th/cmp-buffer"
-	use "hrsh7th/cmp-cmdline"
+	'hrsh7th/nvim-cmp',
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/cmp-path',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-cmdline',
+	'hrsh7th/vim-vsnip',
 
 	-- airline
-	use 'vim-airline/vim-airline'
+	'vim-airline/vim-airline',
 
 	-- vim-devicons
-	use 'ryanoasis/vim-devicons'
+	'ryanoasis/vim-devicons',
 
 	-- nerdtree
-	use 'scrooloose/nerdtree'
+	'scrooloose/nerdtree',
 
 	-- neoterm
-	use 'kassio/neoterm'
+	'kassio/neoterm',
 
 	-- git command
-	use 'tpope/vim-fugitive'
-	use 'airblade/vim-gitgutter'
+	'tpope/vim-fugitive',
+	'airblade/vim-gitgutter',
 
 	-- gcc
-	use 'tpope/vim-commentary'
+	'tpope/vim-commentary',
 
 	-- rust
-	use 'rust-lang/rust.vim'
+	-- 'rust-lang/rust.vim',
 
-	-- lspUI
-	use({
-		"glepnir/lspsaga.nvim",
-		branch = "main",
+	-- lsp saga
+	{
+		'nvimdev/lspsaga.nvim',
 		config = function()
 			require('lspsaga').setup({})
 		end,
-	})
-end)
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter', -- optional
+			'nvim-tree/nvim-web-devicons' -- optional
+		}
+	}
+})
+
+vim.encoding = "UTF-8"
 
 -- 行番号
 vim.opt.number = true
@@ -85,7 +111,7 @@ vim.opt.incsearch = true
 vim.opt.clipboard = 'unnamedplus'
 
 --マウス
-vim.opt.mouse = "a"
+vim.opt.mouse = 'a'
 
 -- cmp
 vim.opt.completeopt = { 'menuone', 'noinsert' }
@@ -109,7 +135,7 @@ let g:airline#extensions#tabline#enabled = 1
 let g:airline#extensions#tabline#left_sep = ' '
 let g:airline#extensions#tabline#left_alt_sep = '|'
 let g:airline#extensions#tabline#buffer_idx_mode = 1
-let g:airline_powerline_fonts = 1
+" let g:airline_powerline_fonts = 1
 let g:airline_theme = 'sonokai'
 ]]
 
@@ -133,15 +159,10 @@ let g:neoterm_default_mod = 'vertical belowright'
 let g:neoterm_autoinsert = 1
 ]]
 
--- vim-fugitive
-vim.cmd [[
-let g:rustfmt_autosave = 1
-]]
-
 -- LSP
 set('n', 'K', '<cmd>Lspsaga hover_doc<CR>')
 
-set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+set('n', 'gf', '<cmd>lua vim.lsp.buf.format()<CR>')
 
 set('n', 'gR', '<cmd>Lspsaga lsp_finder<CR>')
 
@@ -151,7 +172,7 @@ set('n', 'gD', '<cmd>Lspsaga peek_definition<CR>')
 
 set('n', 'gn', '<cmd>Lspsaga rename<CR>')
 
-set({ "n", "v" }, "ga", "<cmd>Lspsaga code_action<CR>")
+set({ 'n', 'v' }, 'ga', '<cmd>Lspsaga code_action<CR>')
 
 set('n', '<space>e', '<cmd>Lspsaga show_line_diagnostics<CR>')
 
@@ -174,17 +195,16 @@ require('mason-lspconfig').setup_handlers({ function(server)
 		)
 	}
 	require('lspconfig')[server].setup(opt)
-
 end })
 
 -- LSP handlers
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = false,
-	signs = true,
-	float = { border = "single" },
-	underline = true,
-}
+		virtual_text = false,
+		signs = true,
+		float = { border = 'single' },
+		underline = true,
+	}
 )
 -- vim.cmd([[au CursorHold * lua Lspsaga show_cursor_diagnostics]])
 
@@ -202,41 +222,68 @@ augroup END
 ]]
 
 -- completion (hrsh7th/nvim-cmp)
-local cmp = require("cmp")
+local cmp = require('cmp')
 cmp.setup({
 	snippet = {
+		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
-	sources = {
-		{ name = "nvim_lsp" },
-		-- { name = "buffer" },
-		-- { name = "path" },
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-n>'] = cmp.mapping.select_next_item(),
 		['<C-l>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm { select = true },
+		['<CR>'] = cmp.mapping.confirm { select = true },
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
 	}),
 	experimental = {
 		ghost_text = true,
 	},
 })
-cmp.setup.cmdline('/', {
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
 		{ name = 'buffer' }
 	}
 })
-cmp.setup.cmdline(":", {
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
 	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "path" },
-		{ name = "cmdline" },
-	},
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
 })
 
 -- treesitter
