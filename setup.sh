a0c032c5a9fb235b01dec8c86f150c1d242de833
nop <noplab90@gmail.com>
2025-08-02 12:58:54 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-sudo pacman -S git curl wget unzip
+# Update local
+pacman -Syy
+
+# Basic packages
+pacman -S git curl wget unzip
+
+# Useful tools
+pacman -S vim tmux
 
 # uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
